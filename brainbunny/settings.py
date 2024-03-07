d7c25107ef1eb57178a3999df5864834af6b92f8
lanabcde <144417094+lanabcde@users.noreply.github.com>
2024-03-07 11:30:28 +0800

    Update settings.py

--- brainbunny/settings.py
+++ brainbunny/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'brainbunny.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fGG6*5DAGB5B12g62ef456E4EfcGcFaE',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '35336',
     }
 }
 
